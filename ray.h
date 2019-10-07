c763bd674aa06617054f41af19a02fe57286c2ca
Daria Kolodzey <kolodaria@gmail.com>
2019-10-07 17:46:48 +0300

    Create ray.h

---
+++ ray.h
@@ -0,0 +1,16 @@
+#ifndef RAYH
+#define RAYH
+#include "vec3.h"
+
+struct Ray {
+  Ray(): A(), B() {}
+  Ray(const Vec3 &a, const Vec3 &b): A(a), B(b) {}
+  Vec3 origin() const { return A; }
+  Vec3 direction() const {return B; }
+  Vec3 point_at_parameter(float t) const {return A + t * B; }
+
+  Vec3 A;
+  Vec3 B;
+};
+
+#endif
