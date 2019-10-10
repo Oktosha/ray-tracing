6bee8854a076c1ec5f7c1a4b30c9220e968fa156
Daria Kolodzey <kolodaria@gmail.com>
2019-10-10 11:30:17 +0200

    [ch 4] Add sphere

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 
 using namespace std;
 
+bool hit_sphere(const Vec3& center, float radius, const Ray& r) {
+  Vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = 2.0 * dot(oc, r.direction());
+  float c = dot(oc, oc) - radius * radius;
+  float discriminant = b * b - 4 * a * c;
+  return discriminant > 0;
+}
+
 Vec3 color(const Ray& r) {
+  if (hit_sphere(Vec3(0, 0, -1), 0.5, r)) {
+    return Vec3(1, 0, 0);
+  }
   Vec3 unit_direction = unit_vector(r.direction());
   float t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0);
