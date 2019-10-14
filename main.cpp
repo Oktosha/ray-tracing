fab9b8a753928bf13368c068924effeb16272266
Daria Kolodzey <kolodaria@gmail.com>
2019-10-14 18:53:46 +0300

    [ch 5] Return parameter value from hit_sphere

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <optional>
 #include "ray.h"
 
 using namespace std;
 
-bool hit_sphere(const Vec3& center, float radius, const Ray& r) {
+optional<float> hit_sphere(const Vec3& center, float radius, const Ray& r) {
   Vec3 oc = r.origin() - center;
   float a = dot(r.direction(), r.direction());
   float b = 2.0 * dot(oc, r.direction());
   float c = dot(oc, oc) - radius * radius;
   float discriminant = b * b - 4 * a * c;
-  return discriminant > 0;
+  if (discriminant < 0) {
+    return {};
+  }
+  return (-b - sqrt(discriminant)) / (2.0 * a);
 }
 
 Vec3 color(const Ray& r) {
