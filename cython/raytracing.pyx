ef20aff72b7a7e0e506d82b3bd364783132d3b09
Daria Kolodzey <kolodaria@gmail.com>
2019-11-28 01:07:32 +0300

    Add rendering of an example image

--- cython/raytracing.pyx
+++ cython/raytracing.pyx
@@ -2,3 +2,8 @@
 
 def print_hello_to(name):
   print("Hello, {}!".format(name))
+
+def render(h, w):
+  """ Renders example (h, w, 3) image"""
+  image = [[[i / w, j / h, 0.2] for i in range(w)] for j in range(h)]
+  return image
