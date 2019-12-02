a24115bf73097ac61f4465ad66b2058b5ba9b1e3
Daria Kolodzey <kolodaria@gmail.com>
2019-12-02 23:10:34 +0300

    Add non-trivial Sphere construction to Cython

--- src/raytracing.pyx
+++ src/raytracing.pyx
@@ -4,9 +4,18 @@
 from libcpp.memory cimport unique_ptr, make_unique
 from libcpp.vector cimport vector
 
+cdef extern from "vec3.h":
+  cppclass Vec3:
+    Vec3(float x, float y, float z)
+
 cdef extern from "hitable.h":
   cppclass Hitable:
     pass
+
+cdef extern from "hitable_list.h":
+  cppclass OwningHitableList(Hitable):
+    OwningHitableList()
+    vector[unique_ptr[Hitable]] v
 
 cdef extern from "sphere.h":
   cppclass Sphere(Hitable):
@@ -14,6 +23,15 @@ cdef extern from "sphere.h":
 
 cdef extern from "material.h":
   cppclass Material:
+    pass
+
+  cppclass Lambertian:
+    pass
+
+  cppclass Metal:
+    pass
+
+  cppclass Dielectric:
     pass
 
 cdef extern from "camera.h":
@@ -25,6 +43,11 @@ cdef extern from "render.h":
 
 def render():
   cdef Camera camera
-  cdef Sphere sphere
-  image = Render(&sphere, camera, 100, 200)
+  cdef OwningHitableList world
+  world.v.push_back(<unique_ptr[Hitable]>make_unique[Sphere](
+    Vec3(-4, 1, 0),
+    2.0,
+    <unique_ptr[Material]>make_unique[Lambertian](Vec3(0.4, 0.4, 0.8))
+  ))
+  image = Render(&world, camera, 100, 200)
   return image
